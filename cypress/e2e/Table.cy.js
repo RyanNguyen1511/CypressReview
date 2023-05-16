a154542372af5da43aceb938f69c8648fff50537
RyanNguyen1511 <nguyenminhtuan1511@gmail.com>
2023-05-16 11:36:22 -0700

    update tables testing

--- cypress/e2e/Table.cy.js
+++ cypress/e2e/Table.cy.js
@@ -2,22 +2,32 @@
 
 describe('handle Tables',()=>{
 	beforeEach('Login',()=>{
-		cy.visit('https://demo.opencart.com/admin/index.php')
-		cy.get('#input-username').type('demo')
-		cy.get('#input-password').type('demo')
-		cy.get("button[type='submit']").click()
+		cy.visit('https://demo.opencart.com/admin/index.php');
+		cy.get('#input-username').type('demo');
+		cy.get('#input-password').type('demo');
+		cy.get("button[type='submit']").click();
 
-		cy.get('.btn-close').click()
-		// Customer
+		cy.get('.btn-close').click();
+		// Customer --> customer
+
+		cy.get('#menu-customer>a').click();
+		cy.get('#menu-customer>ul>li:first-child').click();
+		
+
+
+
 
 	})
 
 	it('Check number of row and Column',()=>{	
-
+		
+		cy.get("table[class='table table-bordered table-hover']>tbody>tr").should('have.length','10')
+		cy.get("table[class='table table-bordered table-hover']>thead>tr>td").should('have.length','7')
 	})
 
-	it('Check cell data from specific row & column',()=>{	
-		
+	it.only('Check cell data from specific row & column',()=>{	
+		cy.get("table[class='table table-bordered table-hover']>tbody>tr:nth-child(5)>td:nth-child(3)").contains('princytrainings4@gmail.com');
+
 	})
 
 	it('Read all the rows & columns data in the first page',()=>{	

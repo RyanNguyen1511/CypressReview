acc65c19a580f8ad8e672a286d95d4222bdefa5c
RyanNguyen1511 <nguyenminhtuan1511@gmail.com>
2023-05-16 12:17:11 -0700

    update Tables testing

--- cypress/e2e/Table.cy.js
+++ cypress/e2e/Table.cy.js
@@ -25,17 +25,67 @@ describe('handle Tables',()=>{
 		cy.get("table[class='table table-bordered table-hover']>thead>tr>td").should('have.length','7')
 	})
 
-	it.only('Check cell data from specific row & column',()=>{	
+	it('Check cell data from specific row & column',()=>{	
 		cy.get("table[class='table table-bordered table-hover']>tbody>tr:nth-child(5)>td:nth-child(3)").contains('princytrainings4@gmail.com');
 
 	})
 
 	it('Read all the rows & columns data in the first page',()=>{	
-		
+
+		cy.get("table[class='table table-bordered table-hover']>tbody>tr")
+		.each(($row,index,$rows)=>{
+
+			cy.wrap($row).within(()=>{
+
+				cy.get('td').each(($col,index,$cols)=>{
+					cy.log($col.text()); // show Showing 1 to 10 of 12749 (1275 Pages)
+					
+
+				})
+
+			})
+
+
+		});
 	})
 
-	it('Pagination',()=>{	
-		
+	it.only('Pagination',()=>{	
+		// find total number of page
+
+		// cy.get(".col-sm-6.text-end").then((e)=>{
+		// 	let mytext = e.text();
+		// 	let totalpage = mytext.substring(mytext.indexOf("(")+1, mytext.indexOf("Pages")-1)
+		// 	cy.log(' Total number of pages: '+ totalpage);
+
+		// })
+		let totalPages =5;
+
+		for (let i = 1; i<=totalPages;i++){
+			if(totalPages>1){
+				cy.log(' Active page is ==> '+i)
+				cy.get("ul[class='pagination']>li:nth-child("+i+")").click()
+				cy.wait(3000)
+				cy.get("table[class='table table-bordered table-hover']>tbody>tr")
+				.each(($row,index,$rows)=>{
+
+					cy.wrap($row).within(()=>{
+
+						cy.get('td').each(($col,index,$cols)=>{
+							cy.log($col.text()); // show Showing 1 to 10 of 12749 (1275 Pages)
+							
+
+						})
+
+					})
+
+
+				});
+				
+			}
+		}
+
+
+
 	})
 
 })

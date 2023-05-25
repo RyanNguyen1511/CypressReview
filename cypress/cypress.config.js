6fea0106cdd9d4966e313139936fe4d2160378ca
RyanNguyen1511 <nguyenminhtuan1511@gmail.com>
2023-05-24 23:36:03 -0700

    add Page Object folder, Run cypress in command plugin

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,14 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "i7cpbp",
-  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });

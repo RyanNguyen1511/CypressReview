1ada597f14c2c9fae8963e257512717a6e42a665
RyanNguyen1511 <nguyenminhtuan1511@gmail.com>
2023-06-01 11:53:40 -0700

    Add Parsing JSON response section

---
+++ cypress/e2e/APITesting/ParsingJSONResponse.cy.js
@@ -0,0 +1,35 @@
+describe("Parsing JSON Response",()=>{
+	
+	it('Parsing simple JSON response', () => {
+		cy.request({
+			method: 'Get',
+			url: 'https://fakestoreapi.com/products',
+		})
+		.then((response)=>{
+			expect(response.status).to.equal(200);
+			expect(response.body[0].it).to.equal(1);
+			expect(response.body[0].title).to.equal("Fjallraven - Foldsack No.1 Backpack, Fits 15 Laptops");
+			expect(response.body[0].price).to.equal(109.95);
+
+		})
+	});
+
+	it('Parsing simple JSON response', () => {
+
+		let totalPrice =0;
+		cy.request({
+			method: 'Get',
+			url: 'https://fakestoreapi.com/products',
+			qs:{limit:5}
+		})
+		.then((response)=>{
+			expect(response.status).to.equal(200);
+			response.body.forEach(element => {
+				totalPrice +=element.price
+			});
+			expect.fail(totalPrice).to.equal(899.23) // limit 5
+
+		})
+	});
+
+})

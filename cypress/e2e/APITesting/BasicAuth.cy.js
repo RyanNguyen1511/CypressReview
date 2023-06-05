bd035b7d50dad5ede8de0c2bdde1c89f2d60a7d3
RyanNguyen1511 <nguyenminhtuan1511@gmail.com>
2023-06-05 15:23:50 -0700

    Update Basic Authenticaiton section

--- cypress/e2e/APITesting/BasicAuth.cy.js
+++ cypress/e2e/APITesting/BasicAuth.cy.js
@@ -15,9 +15,9 @@ describe('Authentication',()=>{
 			expect(response.body.authenticated).to.eq(true);
 
 		})
-	})
+	}) //it block closed
 
-	it.only("Digest Authentication",()=>{
+	it("Digest Authentication",()=>{
 		cy.request({
 			method: 'GET',
 			url: 'https://postman-echo.com/basic-auth',
@@ -32,5 +32,34 @@ describe('Authentication',()=>{
 			expect(response.body.authenticated).to.eq(true);
 
 		})
-	})
+	}) //it block closed
+
+	const valid_token='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';
+    const Invalid_token='xyz';
+
+    it.only('Bearer Token Authentication', () => {
+      cy.request({
+        method: 'GET',
+        url: 'https://api.github.com/user/repos',
+        headers: {
+          Authorization: 'Bearer '+valid_token
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+      })
+    }) //it block closed
+
+	 //https://openweathermap.org/api
+    it('API Key  Authentication', () => {
+      cy.request({
+        method: 'GET',
+        url: 'api.openweathermap.org/data/2.5/forecast/daily?q=Delhi',
+        qs:{
+          appid:'fe9c5cddb7e01d747b4611c3fc9eaf2c' //API Key & value
+            }
+        }).then((response) => {
+        expect(response.status).to.eq(200)
+      })
+    }) //it block closed
+
 })
